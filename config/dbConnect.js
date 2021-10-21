874e2ecf7dcec9e4843c67ffe92157ab9b8b9430
Sayyed Abrar Akhtar <akhtars10@uni.coventry.ac.uk>
2021-10-21 16:12:02 +0545

    Replaced react with next an react framework

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,15 +1,22 @@
 import mongoose from 'mongoose';
+import colors from 'colors';
 
 const dbConnect = async () => {
   try {
+    if (mongoose.connection.readyState >= 1) {
+      return;
+    }
     const conn = await mongoose.connect(process.env.DB_LOCAL_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    console.log(`MongoDB connected`);
+    console.log(
+      `MongoDB connected: ${conn.connection.host}`.brightMagenta.bold.italic
+        .underline
+    );
   } catch (err) {
-    console.error(`Error: ${err}`);
+    console.error(`Error: ${err}`.bgRed.white.bold.italic.underline);
     process.exit(1); // <-- no more async operations are pending and exit with failure code.
   }
 };
--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,5 +1,22 @@
-const allRooms = (req, res) => {
-  res.status(200).json({ success: true, message: 'All Rooms' });
+import Room from '../models/room';
+
+const allRooms = async (req, res) => {
+  try {
+    const rooms = await Room.find();
+    res.status(200).json({ success: true, rooms });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
 };
 
-export { allRooms };
+// CREATE NEW ROOM => /api/rooms
+const newRoom = async (req, res) => {
+  try {
+    const room = await Room.create(req.body);
+    res.status(200).json({ success: true, room });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
+};
+
+export { allRooms, newRoom };
